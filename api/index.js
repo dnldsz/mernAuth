cdda113ff5bcab7a68fecb72da09e48413bd3fdc
Daniel D'Souza <danieldsouza.bne@gmail.com>
2024-04-04 13:36:29 +1000

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(200, () => {
     console.log('listening on 3000')
 })
+
