cd4e70c956aa3c9ddceaa3c60177b67e7737defe
Daniel D'Souza <danieldsouza.bne@gmail.com>
2024-04-04 13:54:56 +1000

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose
@@ -14,7 +15,8 @@ mongoose
 
 const app = express();
 
-app.listen(200, () => {
-    console.log('listening on 3000')
-})
+app.listen(3000, () => {
+    console.log('listening on 3000');
+});
 
+app.use('/api/user', userRoutes);
